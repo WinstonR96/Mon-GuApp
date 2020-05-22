657f39eed98f54ca8ee45390b1e62a5bfefd998a
Winston Jr Rodriguez Stand <wrodriguez@redsis.com>
2020-05-21 19:50:53 -0500

    Registrar consultorio

--- Mon-GuApp/ClientApp/src/App.js
+++ Mon-GuApp/ClientApp/src/App.js
@@ -7,6 +7,7 @@ import { Paciente } from "./components/Paciente";
 import { NewPaciente } from "./components/NewPaciente";
 import { HistoriaPaciente } from "./components/HistoriaPaciente";
 import { Consultorio } from "./components/Consultorio";
+import { NewConsultorio } from "./components/NewConsultorio";
 import { DetalleConsultorio } from "./components/DetalleConsultorio";
 import { FetchData } from "./components/FetchData";
 import { Counter } from "./components/Counter";
@@ -25,6 +26,7 @@ export default class App extends Component {
         <Route exact path="/paciente/nuevo" component={NewPaciente} />
         <Route exact path="/paciente/detalle" component={HistoriaPaciente} />
         <Route exact path="/consultorio" component={Consultorio} />
+        <Route exact path="/consultorio/nuevo" component={NewConsultorio} />
         <Route
           exact
           path="/consultorio/detalle"
---
+++ Mon-GuApp/ClientApp/src/components/NewConsultorio.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import {
+  Container,
+  Col,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+} from "reactstrap";
+
+export class NewConsultorio extends Component {
+  static displayname = NewConsultorio.name;
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      Codigo: "",
+      Medico: "",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = async (event) => {
+    const { target } = event;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const { name } = target;
+    await this.setState({
+      [name]: value,
+    });
+  };
+
+  submitForm(e) {
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <Container className="App">
+          <h2>Datos del consultorio</h2>
+          <Form className="form" onSubmit={(e) => this.submitForm(e)}>
+            <Col>
+              <FormGroup>
+                <Label for="Codigo">Codigo</Label>
+                <Input
+                  type="number"
+                  name="Codigo"
+                  id="Codigo"
+                  onChange={(e) => this.handleChange(e)}
+                />
+              </FormGroup>
+            </Col>
+            <Col>
+              <FormGroup>
+                <Label for="Medico">Medico</Label>
+                <Input
+                  type="text"
+                  name="Medico"
+                  id="Medico"
+                  onChange={(e) => this.handleChange(e)}
+                />
+              </FormGroup>
+            </Col>
+            <Button>Registrar</Button>
+          </Form>
+        </Container>
+      </div>
+    );
+  }
+}
